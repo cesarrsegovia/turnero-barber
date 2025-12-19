270409d25b5227c813d7740a949f4d64131466ac
cesarrsegovia <cesarr.segovia27@gmail.com>
2025-12-19 18:20:45 -0300

    arreglos

--- src/components/AppointmentList.tsx
+++ src/components/AppointmentList.tsx
@@ -15,7 +15,7 @@ export default function AppointmentList() {
   const handleSearch = async (text: string) => {
     setQuery(text);
   };
-  //asdsd
+  // Debounce search
   useEffect(() => {
     const timer = setTimeout(async () => {
       setLoading(true);
